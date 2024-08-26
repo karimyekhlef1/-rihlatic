e2aed017312c89f56f5dafd53bf335d13e8bcebc
Haroun Rayes <bouzitharoun@gmail.com>
2024-08-26 18:06:09 +0200

    start working on the project

--- app/Layout/Header/Navbar/linkNav.tsx
+++ app/Layout/Header/Navbar/linkNav.tsx
@@ -1,6 +1,7 @@
+import { NavbarItem } from '@/app/Types/Common/navLink';
 import React from 'react';
 
-const LinkNav = ({name, route, isSelected}: {name: string, route: string, isSelected: boolean}) => {
+const LinkNav: React.FC<NavbarItem> = ({name, route, isSelected}) => {
     return (
         <a href="#" className={(`inline-flex items-center ${isSelected ? ('text-[#FF8000] border-b-2 border-[#FF8000]') : 'text-gray-900'} px-1 pt-1 text-sm font-medium`)}>
             {name}
