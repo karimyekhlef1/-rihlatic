0668b275646db4a2a40907b726be04126186c4e6
Nadjib Boubrim <nlbgucci77@gmail.com>
2024-08-28 10:25:46 +0100

    Fixed tips section on mobile

--- components/Main/tips-card.tsx
+++ components/Main/tips-card.tsx
@@ -15,21 +15,23 @@ import codeQR from '@/public/images/code_qr.svg';
 export default function TipsCard() {
   return (
     <div className="flex flex-col">
-      <h1 className="text-2xl font-semibold pb-2 md:pl-8">Smart travel tips</h1>
+      <h1 className="text-2xl font-semibold pb-2 pl-8 sm:pl-8">
+        Smart travel tips
+      </h1>
       <div className="md: px-6">
         <Card>
           <CardContent className="p-0">
-            <div className="flex flex-col md:flex-row">
+            <div className="flex flex-col-reverse md:flex-row">
               <div className="flex-shrink-0">
                 <Image src={mockup} alt="RHTLK" width={300} height={300} />
               </div>
               <div className="flex flex-col mt-4 md:mt-0 md:ml-4">
-                <p className="box-content w-full md:w-[450px] pt-4 md:pt-10 text-xl md:text-3xl font-bold">
+                <p className="box-content font-bold text-center w-full pt-4 text-xl md:w-[450px] md:pt-10 md:text-3xl">
                   Travel smarter. Get our app for exclusive features to navigate
                   the world like a pro.
                 </p>
                 <Link href={'#'}>
-                  <p className="text-lg md:text-xl text-[#FF8000] font-bold pt-6 md:pt-14">
+                  <p className="text-lg text-center md:text-xl text-[#FF8000] font-bold pt-6 md:pt-14">
                     Learn more...
                   </p>
                 </Link>
