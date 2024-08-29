542b3b25889bff26433b25a355e885b19b07a1a5
Nadjib Boubrim <nlbgucci77@gmail.com>
2024-08-29 12:00:12 +0100

    Overflow fixed on mobile

--- app/Layout/Main/accountSettings.tsx
+++ app/Layout/Main/accountSettings.tsx
@@ -7,7 +7,7 @@ import Notifications from '@/components/Main/notifications';
 
 export default function AccountSettings() {
   return (
-    <div className="flex flex-wrap flex-col items-center md:items-start md:flex-row md:justify-center pt-8 pb-20 bg-[#f8f8f8]">
+    <div className="flex overflow-x-auto flex-wrap flex-col items-center md:items-start md:flex-row md:justify-center pt-8 pb-20 bg-[#f8f8f8]">
       <div className="flex flex-col md:px-20">
         <h1 className="font-semibold">Account Settings</h1>
         <p className="box-content font-medium text-[14px] w-[315px] h-[63px] text-gray-500">
