56f7143f9bb24d6894505d383488084e51ccae4c
Nadjib Boubrim <nlbgucci77@gmail.com>
2024-09-04 15:44:01 +0100

    Started working on trip summary

--- app/commonComponents/contentComponent.tsx
+++ app/commonComponents/contentComponent.tsx
@@ -1,11 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 
 const ContentComponent: React.FC<ContentProps> = ({
   content,
