5d328bc302cb7ad3c5a76fd7a8a5e8a074de113b
Nadjib Boubrim <nlbgucci77@gmail.com>
2024-09-05 00:31:38 +0100

    Some minor changes

--- app/Components/packages/bookingPackageComponent.tsx
+++ app/Components/packages/bookingPackageComponent.tsx
@@ -15,12 +15,20 @@ export default function BookingPackageComponent() {
             </div>
             <Separator />
             <div className="flex flex-col pt-4 pb-[100px]">
-              <div className="flex flex-row justify-center items-center">
-                <CircleCheck size={15} color="blue" />
+              <div className="flex flex-row items-center">
+                <CircleCheck
+                  size={15}
+                  className="font-semibold text-xs text-[#43acff]"
+                  fill="#b4deff"
+                />
                 <p className="text-sm font-semibold pl-2">8 nights / 9 days</p>
-              </div>{' '}
-              <div className="flex flex-row justify-center items-center">
-                <CircleCheck size={15} color="orange" />
+              </div>
+              <div className="flex flex-row items-center mt-2">
+                <CircleCheck
+                  size={15}
+                  className="font-semibold text-xs text-[#ff8000]"
+                  fill="#ffcc99"
+                />
                 <p className="text-sm font-semibold pl-2">12-August-2024</p>
               </div>
             </div>
