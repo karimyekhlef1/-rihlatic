29dc67a577965404e712cdebc7c72ad88e1b2257
Nadjib Boubrim <nlbgucci77@gmail.com>
2024-09-07 12:05:15 +0100

    Fixed pagination issues on mobile/tablet view

--- app/[lang]/hotels/page.tsx
+++ app/[lang]/hotels/page.tsx
@@ -9,10 +9,12 @@ import PackagesComponent from '@/app/Components/packages/packagesComponent';
 
 export default function Hotels() {
   return (
-    <div className="flex flex-row">
-      <div className="px-14 flex flex-col items-center pt-10 gap-y-8 pb-10">
+    <div className="flex md:flex-row flex-col">
+      <div className="px-14 flex flex-col items-center pt-10 gap-y-8 md:pb-10">
         <FilterComponent />
-        <AdComponent />
+        <div className="hidden md:block">
+          <AdComponent />
+        </div>
       </div>
       <div className="px-10 pt-10 gap-y-8 pb-10">
         <Provider store={store}>
