5cd4ce9026ed1516f98bc4b938f0e37f1b6290ed
Nadjib Boubrim <nlbgucci77@gmail.com>
2024-09-15 00:14:39 +0100

    minor fix

--- app/Components/search/tripSummary.tsx
+++ app/Components/search/tripSummary.tsx
@@ -43,6 +43,16 @@ export default function TripSummary() {
           </DialogTitle>
         </DialogHeader>
         <TripSummaryComponent />
+        <div className="relative py-4">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border-gray-300"></div>
+          </div>
+          <div className="relative flex justify-center">
+            <span className="bg-white px-2 text-xs font-semibold">
+              12 nights in Paris
+            </span>
+          </div>
+        </div>
         <TripSummaryComponent />
       </DialogContent>
     </Dialog>
