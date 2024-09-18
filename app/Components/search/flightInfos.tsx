1873584a5736851242d0f1873241d45dbea3aa7c
Nadjib Boubrim <nlbgucci77@gmail.com>
2024-09-18 11:19:12 +0100

    Edited travelOptions and made results card smaller

--- app/Components/search/flightInfos.tsx
+++ app/Components/search/flightInfos.tsx
@@ -7,44 +7,44 @@ const FlightInfos: React.FC = () => {
   return (
     <div className="pb-4 relative">
       <div className="flex justify-between items-center mb-2">
-        <span className="text-xs sm:text-sm font-medium text-gray-400">
+        <span className="text-xs font-medium text-gray-400">
           Thu 26 Sep • Outbound
         </span>
       </div>
       <div className="flex justify-between items-center">
         <div className="relative z-10 pr-2 bg-white">
-          <div className="text-xl sm:text-2xl font-semibold">ALG</div>
-          <div className="text-lg sm:text-xl">12:25</div>
+          <div className="text-lg font-semibold">ALG</div>
+          <div className="text-base">12:25</div>
         </div>
         <div className="text-center relative z-10 bg-white">
           <div className="flex items-center space-x-1">
-            <div className="bg-gray-100 rounded-full px-3 sm:px-4 py-1 text-xs sm:text-sm text-black font-medium">
+            <div className="bg-gray-100 rounded-full px-2 py-1 text-xs text-black font-medium">
               2 stops
             </div>
             <div className="flex -space-x-2">
               <Image
                 src={airlogo2}
                 alt="Air France"
-                width={20}
-                height={20}
-                className="relative z-10 sm:w-[25px] sm:h-[25px]"
+                width={18}
+                height={18}
+                className="relative z-10"
               />
               <Image
                 src={airlogo1}
                 alt="Air Algerie"
-                width={20}
-                height={20}
-                className="relative z-20 sm:w-[25px] sm:h-[25px]"
+                width={18}
+                height={18}
+                className="relative z-20"
               />
             </div>
           </div>
-          <div className="text-xs sm:text-sm font-medium text-muted-foreground mt-1">
+          <div className="text-[10px] font-semibold text-muted-foreground mt-1">
             9h 20m
           </div>
         </div>
         <div className="relative z-10 pl-2 bg-white">
-          <div className="text-xl sm:text-2xl font-semibold">MME</div>
-          <div className="text-lg sm:text-xl">21:55</div>
+          <div className="text-lg font-semibold">MME</div>
+          <div className="text-base">21:55</div>
         </div>
       </div>
       <div
--- app/Components/search/flightSeparator.tsx
+++ app/Components/search/flightSeparator.tsx
@@ -5,7 +5,7 @@ const FlightSeparator: React.FC = () => {
     <div>
       <div className="relative py-3">
         <div className="border-t border-dashed border-gray-300"></div>
-        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full px-4 py-1 sm:text-sm text-xs text-black font-medium flex justify-center border border-gray-200">
+        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full px-3 py-0.5 text-xs sm:text-[10px] text-black font-medium flex justify-center border border-gray-200">
           21 nights in England
         </div>
       </div>
