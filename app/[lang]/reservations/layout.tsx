e73342702b96100c5c707ad408b3662cf6a8d44c
karim yekhlef <76750881+karimyekhlef1@users.noreply.github.com>
2024-11-16 15:37:39 +0100

    first commit karim branch

--- app/[lang]/reservations/layout.tsx
+++ app/[lang]/reservations/layout.tsx
@@ -1,7 +1,7 @@
 import Footer from '@/app/commonComponents/footer';
 import Navbar from '@/app/Layout/Header/Navbar/navbar';
 import MainNavbar from '@/app/Layout/Main/mainNavbar';
-
+// first commit karim branch "0.0.0" 
 const ReservationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
