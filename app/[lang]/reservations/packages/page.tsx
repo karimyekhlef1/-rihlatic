53839a7e0ea2ce003c44f07b92a02de20b6cc383
karim yekhlef <76750881+karimyekhlef1@users.noreply.github.com>
2025-01-02 17:39:49 +0100

    package cancel with penalty

--- app/[lang]/reservations/packages/page.tsx
+++ app/[lang]/reservations/packages/page.tsx
@@ -3,7 +3,6 @@ import PackagesTable from '@/app/Components/profile/PackagesTable'
 export default function page() {
   return (
     <div className="h-full flex-col items-center flex pt-8 pb-20 bg-[#f8f8f8]">
-
       <div className="flex pt-8">
         <SearchInput />
       </div>
