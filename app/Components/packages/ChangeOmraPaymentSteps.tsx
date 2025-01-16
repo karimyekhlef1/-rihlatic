87b54952c9f99f1e784d0e51d5e79dcdaf1892dd
Nadjib Boubrim <nlbgucci77@gmail.com>
2025-01-16 14:26:43 +0100

    Fixed "Back" button

--- app/Components/packages/ChangeOmraPaymentSteps.tsx
+++ app/Components/packages/ChangeOmraPaymentSteps.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   nextStep,
   previousStep,
 } from "@/lib/store/custom/omraSlices/paymentOmraSlice";
+import { RootState } from "@/lib/store/store";
 
 export default function ChangeOmraPaymentSteps() {
   const dispatch = useDispatch();
+  const currentStep = useSelector((state: RootState) => state.paymentOmra.currentStep);
 
   const handleNext = () => {
     dispatch(nextStep());
@@ -18,12 +20,17 @@ export default function ChangeOmraPaymentSteps() {
   };
 
   return (
-    <div className="flex justify-between w-1/2  mx-auto my-4">
-      <Button variant="outline" onClick={handleBack} className="px-16">
-        Back
-      </Button>
-
-      <Button variant={"active"} onClick={handleNext} className="px-16">
+    <div className="flex justify-between w-1/2 mx-auto my-4">
+      {currentStep > 1 && (
+        <Button variant="outline" onClick={handleBack} className="px-16">
+          Back
+        </Button>
+      )}
+      <Button 
+        variant="active" 
+        onClick={handleNext} 
+        className={`px-16 ${currentStep === 1 ? "ml-auto" : ""}`}
+      >
         Next
       </Button>
     </div>
