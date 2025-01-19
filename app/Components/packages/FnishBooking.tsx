5d543af1792fe30159294d76291b08494bc97036
karim yekhlef <76750881+karimyekhlef1@users.noreply.github.com>
2025-01-19 21:50:19 +0100

    Update FnishBooking.tsx

--- app/Components/packages/FnishBooking.tsx
+++ app/Components/packages/FnishBooking.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+"use client"
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Button } from "@/components/ui/button";
