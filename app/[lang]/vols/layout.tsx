044cb3dff0a376b30276893ea895460816f68417
Nadjib Boubrim <nlbgucci77@gmail.com>
2025-01-22 14:10:36 +0100

    Fixed minor layout issue

--- app/[lang]/vols/layout.tsx
+++ app/[lang]/vols/layout.tsx
@@ -6,15 +6,15 @@ import Navbar from "@/app/Layout/Header/Navbar/navbar";
 const VolsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className=" top-0 z-50 w-full bg-white">
+      <div className="fixed top-0 z-50 w-full bg-white">
         <Navbar />
       </div>
-      <div className=" top-[64px] z-40 w-full bg-white shadow-sm">
+      <div className="fixed top-[64px] z-50 w-full bg-white shadow-sm">
         <div className="container mx-auto py-4">
           <VolSearchComponent />
         </div>
       </div>
-      <main className="min-h-screen bg-[#F8F8F8]">
+      <main className="pt-[160px] min-h-screen bg-[#F8F8F8]">
         <div className="container mx-auto py-6">{children}</div>
       </main>
       <Footer />
