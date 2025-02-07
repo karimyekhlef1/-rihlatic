4bd2cfc9352de701f569b996237080d52c0d7e3d
karim yekhlef <76750881+karimyekhlef1@users.noreply.github.com>
2025-02-07 16:52:10 +0100

    updates of Favorites Destinations

--- app/Components/home/favSection.tsx
+++ app/Components/home/favSection.tsx
@@ -14,8 +14,8 @@ const FavSection= ({data}: {data:any}) => {
                 <SectionHeader title='Favorites Destinations' description='lorem ipsum dolor sit amet consectetur adipiscing elit.' />
             </div>
             <div className="mx-auto mt-5 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-4 lg:gap-8">
-                {data?.map((person:any) => (
-                    <DiscoverComponent key={person.id} image={person.image} name={person.name} establishment={10} />
+                {data?.map((destination:any) => (
+                    <DiscoverComponent key={destination.id} image={destination.image} name={destination.name} establishment={destination.departures_count} />
                 ))}
             </div>
         </div>
